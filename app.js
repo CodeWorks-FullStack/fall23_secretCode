08fc10bc6a6c0f154547eb9d21128cbbf61d6ad8
Jeremy <jembyfowler@gmail.com>
2023-09-25 15:15:04 -0600

    added notes

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
+// NOTE creates a group of your console logs, ends the section by calling console.groupEnd()
 console.group('examples')
 
 console.log('javascript is working!')
 
 // SECTION strings
 
+// NOTE let creates a variable that's value can change
 let firstName = 'Jeremy'
+// NOTE const creates a variable that's value cannot change
+const lastName = 'Fowler'
 
 // NOTE outdated way to declare variables
 var middleName = 'Bob'
 
-const lastName = 'Fowler'
 
 firstName = 'Jerms'
 
-const catchPhrase = `How you doin' "darlin"`
+// NOTE double-quotes can be used to create strings containing single quotes
+const catchPhrase = "How you doin' "
 
-// throws error
+// NOTE backticks can be used to create strings containing single quotes and double quotes
+const extendedCatchPhrase = `How you doin' "darlin"`
+
+// NOTE throws error, lastame is a constant
 // lastName = 'CatDaddy'
 
+// NOTE string concatenation adds strings together
 console.log('My name is ' + firstName + ' ' + middleName + ' ' + lastName + '!');
 
+// NOTE string interpolation injects values inside of strings, must use backticks and ${} to interpolate
 console.log(`My name is ${firstName} ${middleName} ${lastName}!`)
 
 // !SECTION
 
 // SECTION boolean
 
+// NOTE different casing examples. camelCase is the standard for js
 // let PascalCase = ''
 // let snake_case = ''
 
@@ -33,17 +43,21 @@ let jeremyLikesCats = true
 
 let jeremyLikesTomatoes = false
 
-if (jeremyLikesCats == true) {
+
+
+if (jeremyLikesCats) {
   console.log('💘 🐈🐈🐈🐈🐈🐈🐈');
 }
 
-if (jeremyLikesCats) {
+// Note this functions exactly the same as the code above
+if (jeremyLikesCats == true) {
   console.log('💘 🐈🐈🐈🐈🐈🐈🐈');
 }
 
 if (jeremyLikesTomatoes) {
   console.log('💘 🍅');
 }
+// NOTE else will run if the above if statement evaluates to false
 else {
   console.log('⚔️ 🍅');
 }
@@ -80,16 +94,20 @@ num2 /= 2
 
 num2 *= 10
 
+// NOTE review PEMDAS, or order of operations
 num2 = (num2 + 25) / 2
 
 console.log(`Here is the value of num2: ${num2}`);
 
 console.groupEnd()
 
+// ANCHOR application code starts here
 
 // SECTION global variables
+// NOTE these variables are accessible throughout all of the code
 const secretMessage = '🧀🍔🍲'
 
+// NOTE sets up a placeholder string that we can add characters to
 let userInput = ''
 
 // !SECTION
@@ -98,15 +116,20 @@ let userInput = ''
 
 // NOTE definition of the function
 function selectCheese() {
+  // NOTE concats this string to the userInput string
   userInput += '🧀'
   console.log('Selected Cheese!', userInput);
   const userInputElement = document.getElementById('currentUserInput')
   userInputElement.innerText = userInput
 }
 
+// NOTE calls the function, tells the function to run
+// selectCheese()
+
 function selectPizza() {
   userInput += '🍕'
   console.log('Selected Pizza!', userInput);
+  // NOTE the selectPizza function calls the updateUserInput function, functions can call other functions
   updateUserInput()
 }
 
@@ -116,6 +139,7 @@ function selectShortcake() {
   updateUserInput()
 }
 
+// NOTE this function has a parameter, and it's value will be assigned when the function is called, check the HTML to see an example of an argument. If no argument is supllied when this function is called, foodItem will be undefined
 function selectFoodItem(foodItem) {
   userInput += foodItem
   console.log('Selected Food Item!', userInput);
@@ -123,27 +147,34 @@ function selectFoodItem(foodItem) {
 }
 
 function updateUserInput() {
+  // NOTE grabs a piece of HTML from the DOM with id of 'currentUserInput'
+  // NOTE if no HTML element has the supplied id, it will return null
   const userInputElement = document.getElementById('currentUserInput')
+  // NOTE changes the text inside of the HTML element
   userInputElement.innerText = userInput
 }
 
 function checkUserInputAgainstSecretCode() {
+  // NOTE this re-assigns the variable, make sure you use the correct amount of "="
+  // if (userInput = secretMessage) {
   if (userInput == secretMessage) {
     console.log('you got the secret code!');
     const secretImageElement = document.getElementById('secretImage')
 
+    // NOTE sets the HTML to this string of HTML in javascript
     secretImageElement.innerHTML = '<img src="https://media1.giphy.com/media/Zu6AATBpCeUzm/giphy.webp?cid=ecf05e4703y5m1bzhvqjwqopyjs157ug8l2aaj3ra59yojoo&ep=v1_gifs_search&rid=giphy.webp&ct=g" alt="secret image">'
   }
   else {
     console.log('you are so dumb!');
+    // NOTE window targets the web browser, and the alert function has a tiny alert window pop up with our supplied text
     window.alert('YOU ARE THE DUMBEST LITTLE DOGGY EVER')
+    // NOTE resets the string
     userInput = ''
     updateUserInput()
   }
 }
 
-// NOTE calls the function, tells the function to run
-// selectCheese()
+
 
 // const selectCheese = () => {
 
@@ -151,14 +182,20 @@ function checkUserInputAgainstSecretCode() {
 
 // !SECTION
 
+// REVIEW truthy falsy in javascript
+// https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+// https://developer.mozilla.org/en-US/docs/Glossary/Falsy
 
-// let emptyString = ''
-// let zero = -1
-// let weird;
+let emptyString = '' // falsy
+let char = 'b' // truthy
+let num3 = -1 // truthy
+let num4 = 0 // falsy
+let weird; // undefined, falsy
+
 
 // if (weird) {
 //   console.log('truthy');
 // }
 // else {
-//   console.log('falsey');
+//   console.log('falsy');
 // }
